1c65581c2fd6b539375d1d50a7623cbbe35c909f
ICanel12 <ivan.canel12@gmail.com>
2021-07-05 23:00:58 -0600

    factorizacion

--- shelly/assets/js/formulario.js
+++ shelly/assets/js/formulario.js
@@ -78,20 +78,111 @@ const preguntas = [
   {
     pregunta: "9. m(a-b)+(a-b)n",
     respuestas: {
-      a: "(a-b)(m+n)",
-      b: "(a+b)(m+n)",
-      c: "(a-b)(m-n)",
+      a: "(a-b) (m+n)",
+      b: "(a+b) (m+n)",
+      c: "(a-b) (m-n)",
     },
     respuestaCorrecta: "a",
   },
   {
     pregunta: "10. b(a+c)-(a+c)l",
     respuestas: {
-      a: "(a-c)(a+l)",
-      b: "(a+c)(a+l)",
+      a: "(a-c) (a+l)",
+      b: "(a+c) (a+l)",
       c: "Ninguna es correcta",
     },
     respuestaCorrecta: "c",
+  },
+
+  {
+    pregunta: "11. x<sup>2</sup>-y<sup>2</sup>",
+    respuestas: {
+      a: "(x+y) (x+y)",
+      b: "(x+y) (x-y)",
+      c: "(x-y) (x-y)",
+    },
+    respuestaCorrecta: "b",
+  },
+  {
+    pregunta: "12. x<sup>6</sup>-4",
+    respuestas: {
+      a: "(x<sup>3</sup>+2) (x<sup>3</sup>-2)",
+      b: "(x<sup>3</sup>+2) (x<sup>3</sup>+2)",
+      c: "Ninguna es correcta",
+    },
+    respuestaCorrecta: "a",
+  },
+  {
+    pregunta: "13. b<sup>2</sup>-1",
+    respuestas: {
+      a: "Ninguna es correcta",
+      b: "(b+1) (b+1)",
+      c: "(b+1) (b-1)",
+    },
+    respuestaCorrecta: "c",
+  },
+  {
+    pregunta: "14. 27x<sup>3</sup>+8",
+    respuestas: {
+      a: "(3x+2) (9x<sup>2</sup>+6X+4)",
+      b: "(3x-2) (9x<sup>2</sup>+6X+4)",
+      c: "(3x+2) (9x<sup>2</sup>+6X-4)",
+    },
+    respuestaCorrecta: "a",
+  },
+  {
+    pregunta: "15. m<sup>6</sup>-216",
+    respuestas: {
+      a: "(m<sup>2</sup>-6) (m<sup>4</sup>+6m<sup>2</sup>+36)",
+      b: "(m<sup>2</sup>+6) (m<sup>4</sup>+6m<sup>2</sup>+36)",
+      c: "Ninguna es correcta",
+    },
+    respuestaCorrecta: "a",
+  },
+  {
+    pregunta: "16. x<sup>15</sup>+64y<sup>3</sup>",
+    respuestas: {
+      a: "(x<sup>15</sup>+4y) (x<sup>10</sup>+4x<sup>5</sup>y+16y<sup>2</sup>)",
+      b: "Ninguna es correcta",
+      c: "(x<sup>15</sup>+4y) (x<sup>10</sup>-4x<sup>5</sup>y+16y<sup>2</sup>)",
+    },
+    respuestaCorrecta: "c",
+  },
+  {
+    pregunta: "17. x<sup>2</sup>-2x+1",
+    respuestas: {
+      a: "(x-1)<sup>2</sup>",
+      b: "(x-2)<sup>2</sup>",
+      c: "(x+1)<sup>2</sup>",
+    },
+    respuestaCorrecta: "a",
+  },
+  {
+    pregunta: "18. x<sup>2</sup>-6x+9",
+    respuestas: {
+      a: "(x+3)<sup>2</sup>",
+      b: "(x-9)<sup>2</sup>",
+      c: "(x-3)<sup>2</sup>",
+    },
+    respuestaCorrecta: "c",
+  },
+  {
+    pregunta: "19. 27x<sup>3</sup>+8",
+    respuestas: {
+      a: "(3x+2) (9x<sup>2</sup>+6X+4)",
+      b: "(3x-2) (9x<sup>2</sup>+6X+4)",
+      c: "(3x+2) (9x<sup>2</sup>+6X-4)",
+    },
+    respuestaCorrecta: "a",
+  },
+  {
+    pregunta: "20. x<sup>2</sup>+14x+49",
+    respuestas: {
+      a: "(x+7)<sup>2</sup>",
+      b: "(x-7)<sup>2</sup>",
+      c: "Ninguna es correcta",
+    },
+    respuestaCorrecta: "a",
   },
 ];
 
